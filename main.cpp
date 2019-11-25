5e3dff31d514865a4218abcb12e5c1b4bdbe4e5e
michaelgerar <49957350+michaelgerar@users.noreply.github.com>
2019-11-24 20:14:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 int main() {
   vector<Postulantes> datos_importantes;
   vector<Postulantes> BaseDeDatos;
-  Postulantes Postulante1("mafe",23,"a",'-','f',"234","asfdgb","sdfgb",true,BaseDeDatos);
+  Postulantes Pformulario(BaseDeDatos);
+  Postulantes Postulante1("mafe",23,"a",'-','f',"234","asfdgb","sdfgb",false,BaseDeDatos);
   Postulantes Postulante2("daniel ",23,"ab",'+','f',"234","asfdgb","s646b",true,BaseDeDatos);
   Postulantes Postulante3("michael",23,"ab",'-','f',"234","asfdgb","s646b",true,BaseDeDatos);
-  Postulantes Postulante4("santiago",23,"a",'+','f',"234","asfdgb","s646b",true,BaseDeDatos);
+  Postulantes Postulante4("santiago",23,"a",'+','f',"234","asfdgb","s646b",false,BaseDeDatos);
   Postulantes Postulante5("natsu",23,"b",'-','f',"234","asfdgb","s646b",true,BaseDeDatos);
   Postulantes Postulante6("elsa",23,"b",'+','f',"234","asfdgb","s646b",true,BaseDeDatos);
-  Postulantes Postulante7("luna",23,"a",'-','f',"234","asfdgb","s646b",true,BaseDeDatos);
+  Postulantes Postulante7("luna",23,"a",'-','f',"234","asfdgb","s646b",false,BaseDeDatos);
   Postulantes Postulante8("lucyfer",23,"o",'+','f',"234","asfdgb","s646b",true,BaseDeDatos);
   Postulantes Postulante9("Cejaz Negraz",23,"o",'-','f',"234","asfdgb","s646b",true,BaseDeDatos);
 
